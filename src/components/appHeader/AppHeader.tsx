104b6cd02d5c1920b59a2b5c2f274a09a7f58578
Vitalina Puzynia <vitalina.p@leadsor.com>
2024-11-28 12:08:35 +0100

    feat: Added buttons to the appHeader (layout only)

--- src/components/appHeader/AppHeader.tsx
+++ src/components/appHeader/AppHeader.tsx
@@ -1,4 +1,14 @@
 import CollectDataButton from "../collectDataButton/collectDataButton";
+import { Button, InputAdornment, TextField } from "@mui/material";
+import {
+    AnalyticsOutlined,
+    BugReport,
+    DoDisturbAlt,
+    DriveFileRenameOutlineRounded,
+    FilterAlt,
+    Settings,
+    Stop,
+} from "@mui/icons-material";
 import logo from "../../assets/logo.svg";
 
 import "./AppHeader.scss";
@@ -6,10 +16,59 @@ import "./AppHeader.scss";
 const AppHeader = () => {
     return (
         <div className="app__header">
-            <div>
+            <div className="header__logo">
                 <img src={logo} alt="logo" />
             </div>
-            <CollectDataButton />
+
+            <div className="header__title">
+                <TextField
+                    placeholder="Untitled workflow"
+                    variant="outlined"
+                    size="small"
+                    slotProps={{
+                        input: {
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <DriveFileRenameOutlineRounded />
+                                </InputAdornment>
+                            ),
+                        },
+                    }}
+                />
+            </div>
+
+            <div className="header__btns">
+                <Button variant="outlined" onClick={(e) => console.log(e.target)} color="warning">
+                    <Stop fontSize="small" sx={{ marginRight: 1 }} />
+                    Stopped
+                </Button>
+
+                <CollectDataButton />
+
+                <Button variant="outlined" onClick={(e) => console.log(e.target)} color="info">
+                    <BugReport fontSize="small" sx={{ marginRight: 1 }} />
+                    Test
+                </Button>
+                <Button variant="outlined" onClick={(e) => console.log(e.target)} color="info">
+                    <AnalyticsOutlined fontSize="small" sx={{ marginRight: 1 }} />
+                    Analytics
+                </Button>
+                <Button variant="outlined" onClick={(e) => console.log(e.target)} color="info">
+                    <Settings fontSize="small" sx={{ marginRight: 1 }} />
+                    Options
+                </Button>
+                <Button variant="outlined" onClick={(e) => console.log(e.target)} color="info">
+                    <FilterAlt fontSize="small" sx={{ marginRight: 1 }} />
+                    Filter
+                </Button>
+            </div>
+            <Button
+                variant="outlined"
+                onClick={(e) => console.log(e.target)}
+                sx={{ color: "dimgray", borderColor: "dimgray" }}>
+                <DoDisturbAlt fontSize="small" sx={{ marginRight: 1 }} />
+                Leave
+            </Button>
         </div>
     );
 };
