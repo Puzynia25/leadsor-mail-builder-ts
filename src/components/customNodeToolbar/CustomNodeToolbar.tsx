f7116ec24a2dc20159f15397cf29e30fb6099557
Vitalina Puzynia <vitalina.p@leadsor.com>
2024-12-03 17:02:51 +0100

    feat: Added functionality for copying node

--- src/components/customNodeToolbar/CustomNodeToolbar.tsx
+++ src/components/customNodeToolbar/CustomNodeToolbar.tsx
@@ -1,12 +1,35 @@
 import { Divider, IconButton } from "@mui/material";
 import { ContactsOutlined, ContentCopyOutlined, DeleteOutlined, SettingsOutlined } from "@mui/icons-material";
 import { NodeToolbar, useReactFlow } from "@xyflow/react";
-import { CommonNodeData } from "../nodes/Node.types";
+import { CommonNodeData, CustomNodeType } from "../nodes/Node.types";
+import { v4 as uuidv4 } from "uuid";
 
 import "./CustomNodeToolbar.scss";
 
 const CustomNodeToolbar = ({ nodeId, data, selected }: { nodeId: string; data: CommonNodeData; selected: boolean }) => {
     const { setNodes } = useReactFlow();
+
+    const onCopyNode = () => {
+        const copyNode = (nodes: CustomNodeType[], nodeId: string): CustomNodeType[] => {
+            const nodeToCopy = nodes.find((node) => node.id === nodeId);
+
+            if (!nodeToCopy) return nodes;
+
+            const newNode = {
+                ...nodeToCopy,
+                id: uuidv4(),
+                position: {
+                    x: nodeToCopy.position.x + 220,
+                    y: nodeToCopy.position.y + 120,
+                },
+                selected: false,
+            };
+
+            return [...nodes, newNode];
+        };
+
+        setNodes((currentNodes) => copyNode(currentNodes, nodeId));
+    };
 
     const onDeleteNode = () => {
         setNodes((nodes) => nodes.filter((node) => node.id !== nodeId));
@@ -18,7 +41,7 @@ const CustomNodeToolbar = ({ nodeId, data, selected }: { nodeId: string; data: C
                 <IconButton onClick={data.onEdit}>
                     <SettingsOutlined className="node-toolbar__icon" />
                 </IconButton>
-                <IconButton onClick={(e) => console.log("copy")}>
+                <IconButton onClick={onCopyNode}>
                     <ContentCopyOutlined fontSize="small" className="node-toolbar__icon" />
                 </IconButton>
                 <IconButton onClick={(e) => console.log("contacts")}>
